d6ee238582c3016f10764d37d1b9c53039897e2b
JeongSangMyeong <ajflsp@naver.com>
2024-10-07 17:37:36 +0900

    qlrm 버전 업그레이드 및 구독자 정보 리스트 추가

--- photogram/src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ photogram/src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -1,11 +1,17 @@
 package com.cos.photogramstart.service;
 
+import java.util.List;
+
+import org.qlrm.mapper.JpaResultMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
 import com.cos.photogramstart.handler.ex.CustomApiException;
+import com.cos.photogramstart.web.dto.subscribe.SubscribeDto;
 
-import jakarta.transaction.Transactional;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -13,6 +19,34 @@ import lombok.RequiredArgsConstructor;
 public class SubscribeService {
 
 	private final SubscribeRepository subscribeRepository;
+	private final EntityManager em; // Repository는 EntityManager를 구현해서 만들어져 있는 구현체
+
+	@Transactional(readOnly = true)
+	public List<SubscribeDto> 구독리스트(int principalId, int pageUserId) {
+
+		StringBuffer sb = new StringBuffer();
+		sb.append("SELECT u.id, u.username, u.profileImageUrl, ");
+		sb.append("if((SELECT 1 FROM subscribe WHERE fromUserId = ? AND toUserId = u.id), 1, 0) subscribeState, ");
+		sb.append("if((?=u.id), 1, 0) equalUserState ");
+		sb.append("FROM user u JOIN subscribe s ");
+		sb.append("ON u.id = s.toUserId ");
+		sb.append("WHERE s.fromUserId = ?"); // 세미콜론 지워야함.
+
+		// 1. 두 번째 줄 물음표 - 로그인 한 아이디(principalId)
+		// 2. 세 번째 줄 물음표 - 로그인 한 아이디(principalId)
+		// 3. 마지막 물음표 pageUserId
+		// 쿼리 완성부분
+		Query query = em.createNativeQuery(sb.toString())
+				.setParameter(1, principalId)
+				.setParameter(2, principalId)
+				.setParameter(3, pageUserId);
+
+		// 쿼리 실행
+		JpaResultMapper result = new JpaResultMapper();
+		List<SubscribeDto> subscribeDtos = result.list(query, SubscribeDto.class); // 여러 건을 받을 거기 때문에 list
+
+		return subscribeDtos;
+	}
 
 	@Transactional
 	public void 구독하기(int fromUserId, int toUserId) {
