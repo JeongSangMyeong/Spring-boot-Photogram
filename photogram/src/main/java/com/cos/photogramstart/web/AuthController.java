020400e0c372f10f48048cd092bbd861059add8b
JeongSangMyeong <ajflsp@naver.com>
2024-11-25 16:11:30 +0900

    유효성 검사 자동화 AOP 처리 구현

--- photogram/src/main/java/com/cos/photogramstart/web/AuthController.java
+++ photogram/src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,18 +1,13 @@
 package com.cos.photogramstart.web;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.cos.photogramstart.domain.user.User;
-import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.service.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
 
@@ -30,9 +25,9 @@ public class AuthController {
 	// 컨트롤러가 붙어있으면 스프링이 컨테이너에 관리하고 있는 메모리에 로드하는데, 객체 생성을 위해서는 생성자를 실행시켜야됨 그래서 됨.
 	// 생성자를 실행 하려고 하는데 자기가 이미 IoC에 등록한 애들 중에서 Service 타입이 있는지 확인하고 있으면 집어넣어줌. 그걸 의존성
 	// 주입 근데 좀 귀찮아서 위에 private AuthService authService;를 조작함
-//	public AuthController(AuthService authService) {
-//		this.authService = authService;
-//	}
+	// public AuthController(AuthService authService) {
+	// this.authService = authService;
+	// }
 
 	@GetMapping("/auth/signin")
 	public String signinForm() {
@@ -49,28 +44,12 @@ public class AuthController {
 	// 회원가입 버튼 -> /auth/signup -> auth/signin
 	// 회원가입 버튼 x
 	@PostMapping("/auth/signup")
-	public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) { // key = value
-		// (x-www-form-urlencoded)
+	public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) { // key = value // (x-www-form-urlencoded)
 
-		if (bindingResult.hasErrors()) {
-			Map<String, String> errorMap = new HashMap<>();
-
-			for (FieldError error : bindingResult.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-				System.out.println("=========================");
-				System.out.println(error.getDefaultMessage());
-				System.out.println("=========================");
-			}
-			throw new CustomValidationException("유효성 검사 실패함", errorMap);
-		} else {
-			// User <- SignupDto
-			User user = signupDto.toEntity();
-			log.info(user.toString());
-			User userEntity = authService.회원가입(user);
-			System.out.println(userEntity);
-			return "auth/signin";
-
-		}
+		// User <- SignupDto
+		User user = signupDto.toEntity();
+		authService.회원가입(user);
+		return "auth/signin";
 
 	}
 
--- photogram/src/main/java/com/cos/photogramstart/web/api/UserApiController.java
+++ photogram/src/main/java/com/cos/photogramstart/web/api/UserApiController.java
@@ -1,14 +1,11 @@
 package com.cos.photogramstart.web.api;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.cos.photogramstart.config.auth.PrincipalDetails;
 import com.cos.photogramstart.domain.user.User;
-import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.service.SubscribeService;
 import com.cos.photogramstart.service.UserService;
 import com.cos.photogramstart.web.dto.CMRespDto;
@@ -57,18 +53,9 @@ public class UserApiController {
 			BindingResult bindingResult, // 꼭 @Valid가 적혀있는 다음 파라메터에 적용해야 함. 수정되어야 함
 			@AuthenticationPrincipal PrincipalDetails principalDetails) {
 
-		if (bindingResult.hasErrors()) {
-			Map<String, String> errorMap = new HashMap<>();
-
-			for (FieldError error : bindingResult.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			}
-			throw new CustomValidationApiException("유효성 검사 실패함", errorMap);
-		} else {
-			User userEntity = userService.회원수정(id, userUpdateDto.toEntity());
-			principalDetails.setUser(userEntity); // 세션 정보 수정
-			return new CMRespDto<>(1, "회원수정 완료", userEntity); // 응답 시에 userEntity의 모든 getter 함수가 호출되고 JSON으로 파싱하여 응답한다.
-		}
+		User userEntity = userService.회원수정(id, userUpdateDto.toEntity());
+		principalDetails.setUser(userEntity); // 세션 정보 수정
+		return new CMRespDto<>(1, "회원수정 완료", userEntity); // 응답 시에 userEntity의 모든 getter 함수가 호출되고 JSON으로 파싱하여 응답한다.
 
 	}
 
